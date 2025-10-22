a329d1dc95621f751e30831e12bd384234ec1b1c
Bakheet <bakheet@gmail.com>
2025-10-22 12:25:11 +0300

    Initial theme setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+﻿const path = require("path");
+
+module.exports = {
+    entry: {
+        app: ["./src/assets/styles/app.scss", "./src/assets/js/app.js"]
+    },
+    output: {
+        path: path.resolve(__dirname, "public"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader", 
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
+};
